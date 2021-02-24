c99e0ec091f7ad6ae6794dbd7b2bdac6a7d5a2f5
Arvianto Dwi <arvianto.dwi@gmail.com>
2021-02-24 08:43:25 +0700

    🔧 Unwatch build directory on dev

--- webpack/config.dev.js
+++ webpack/config.dev.js
@@ -3,7 +3,6 @@ const { merge } = require("webpack-merge");
 const common = require("./config.common");
 
 const rootDir = path.resolve(__dirname, "../");
-const buildDir = path.join(rootDir, "dist");
 const srcDir = path.join(rootDir, "src");
 
 module.exports = merge(common, {
@@ -13,6 +12,6 @@ module.exports = merge(common, {
 
   devServer: {
     port: 8080,
-    contentBase: [buildDir, srcDir],
+    contentBase: [srcDir],
   },
 });
